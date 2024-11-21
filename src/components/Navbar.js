7f4c731b3521c0af904d6198d4c719bb5eea917a
Hari Krishna Tulisagari <krishpateltulisagari123@gmail.com>
2024-11-21 14:20:29 +0530

    Made responsive changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import Dropdown from "./Dropdown";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen((prevState) => !prevState);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-brand">
         <span className="brand-name">SSR CHEMICALS</span>
       </div>
-      <ul className="navbar-menu">
+
+      {/* Hamburger Menu */}
+      <div className="hamburger-menu" onClick={toggleMenu}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+
+      
+      <ul className={`navbar-menu ${isMenuOpen ? "open" : ""}`}>
         <li>
           <Dropdown title="Products" options={["Basic Chemicals", "Lab Chemicals", "Performance Chemicals", "Vooki"]} />
         </li>
         <li>
-          <Dropdown title="Industry" options={["Sugar Chemicals", "Food Colors", "Oil Field Chemicals", "Leather Chemicals","PU Additives","Pigment Exports"]} />
+          <Dropdown title="Industry" options={["Sugar Chemicals", "Food Colors", "Oil Field Chemicals", "Leather Chemicals", "PU Additives", "Pigment Exports"]} />
         </li>
         <li>
           <Dropdown title="About Us" options={["Who we are", "Our Legacy", "Our Brands & Products"]} />
